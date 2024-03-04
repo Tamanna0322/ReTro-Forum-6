7c2645fd769cff156e5ce8033a27438d516c3a61
Tamanna0322 <tamanna0322@gmail.com>
2024-03-04 13:53:34 +0600

    added latest posts section

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const loadCard = async(cardId) =>{
 
                     </div>
                     <div>
-                        <button onclick="addTitle('${item.title}','${item.view_count}')"
+                        <button onclick="addTitle('${item.title.replace("'", '')}','${item.view_count}')"
                             class="bg-green-500 h-[35px] w-[35px] rounded-full text-white flex justify-center items-center">
                             <i class="fa-solid fa-envelope-open"></i>
                         </button>
@@ -136,4 +136,52 @@ function loadingSpinner(isLoading){
          }
 }
 
+
+const latestCard = async() =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await response.json();
+    // console.log(data);
+    const postContainer = document.getElementById('post-container');
+    data.forEach((item) => {
+        console.log(item);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="border-2 p-4 rounded-2xl">
+        <div class="rounded-2xl">
+            <img class="w-full rounded-2xl" src="${item.cover_image}"
+                alt="">
+        </div>
+        <div class="flex space-x-3 mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 text-[#12132DB3]">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+            </svg>
+            <span class="text-[#12132DB3]">${item?.author?.posted_date ? item.author.posted_date : 'No Publish Date'}</span>
+        </div>
+        <div>
+            <h3 class="text-xl font-bold mt-4">${item.title}</h3>
+        </div>
+        <div>
+            <p class="mt-4 text-[#12132DB3]">${item.description}</p>
+        </div>
+        <div class="mt-4 flex space-x-5">
+            <div class="h-[50px] w-[50px] rounded-full">
+                <img class="rounded-full" src="${item.profile_image}" alt="">
+            </div>
+            <div>
+                <h4 class="font-bold ">${item.author.name}</h4>
+                <p class="text-[#12132DB3]">${item?.author?.designation ? item.author.designation : 'Unknown'}</p>
+            </div>
+        </div>
+        
+    </div>
+        `
+        postContainer.appendChild(div);
+    })
+}
+
+
+
 loadCard('music');
+latestCard();
